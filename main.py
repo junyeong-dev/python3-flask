4ed9112cbf7e4baafbf4c4430f1ded7bb36c07d2
Jun-cloud <kjy8827@gmail.com>
2021-02-02 21:28:10 +0900

    Dynamic URLs and Templates

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("Flask")
 
+# @ : decorator로서 바로 아래에 있는 함수를 찾음
 @app.route("/")
 def home():
-    return "Hello flask world!"
+    # render_template을 통해 html파일을 읽어올 수 있음
+    return render_template("home.html")
+
+# <> <- placeholder를 통해 parameter를 받을 수 있음
+# 받은 인자는 사용해야 에러가 나지 않음
+@app.route("/<name>")
+def name(name):
+    return f"Your name is {name}"
 
 app.run()
